bad5993d35153c15a69bc0e406fead18d65554e3
Zengs <zingsono@gmail.com>
2018-05-03 10:11:41 +0800

    Eureka

--- src/main/java/io/ms/scloud/eureka/Application.java
+++ src/main/java/io/ms/scloud/eureka/Application.java
@@ -1,11 +1,11 @@
 package io.ms.scloud.eureka;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringCloudApplication
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
